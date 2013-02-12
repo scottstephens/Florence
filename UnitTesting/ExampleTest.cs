9bb30c6b3422a4620fcf279ae6bf0910dac6b998
Scott Stephens <stephens.js@gmail.com>
2013-02-12 09:29:36 -0600

    Initial commit from r35 of Sourceforge trunk

---
+++ UnitTesting/ExampleTest.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace NPlot.UnitTesting
+{
+    /// <summary>
+    /// Example Unit Test
+    /// </summary>
+    [TestFixture]
+    public class ExampleTest
+    {
+        // Example Test
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
---
+++ VisualStudio2005_ReadMe.txt
@@ -0,0 +1,8 @@
+The NPlot project is currently targeting the .Net Framework version 1.1.
+
+In order to open this project in Visual Studio 2005 one must first install MSBee.
+
+MSBee is a small tool from Mircosoft that allows Visual Studio 2005 projects to target the v1.1 framework.
+
+To download MSBee go to:
+http://msdn.microsoft.com/vstudio/downloads/tools/msbee/default.aspx
