ee07e60d5946a1895b3d401e8bf21bad70b36037
Scott Stephens <stephens.js@gmail.com>
2013-02-12 21:27:25 -0600

    Change license of odd file out in demo.
    Permission received from Matt Howlett via email.

--- demo/csharp/src/FinancialDemo.cs
+++ demo/csharp/src/FinancialDemo.cs
@@ -1,53 +1,34 @@
 /*
-Florence - A charting library for .NET
-
-FinancialDemo.cs
-Copyright (C) 2003 
-Matt Howlett
-
-Redistribution and use of Florence or parts there-of in source and
-binary forms, with or without modification, are permitted provided
-that the following conditions are met:
-
-1. Re-distributions in source form must retain at the head of each
-   source file the above copyright notice, this list of conditions
-   and the following disclaimer.
-
-2. Any product ("the product") that makes use Florence or parts 
-   there-of must either:
-  
-    (a) allow any user of the product to obtain a complete machine-
-        readable copy of the corresponding source code for the 
-        product and the version of NPlot used for a charge no more
-        than your cost of physically performing source distribution,
-	on a medium customarily used for software interchange, or:
-
-    (b) reproduce the following text in the documentation, about 
-        box or other materials intended to be read by human users
-        of the product that is provided to every human user of the
-        product: 
-   
-              "This product includes software developed as 
-              part of the NPlot library project available 
-              from: http://www.nplot.com/" 
-
-        The words "This product" may optionally be replace with 
-        the actual name of the product.
-
-------------------------------------------------------------------------
-
-THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
-IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
-OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
-IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
-INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
-NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
-THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-*/
+ * Florence - A charting library for .NET
+ * 
+ * FinancialDemo.cs
+ * Copyright (C) 2003-2006 Matt Howlett and others.
+ * Copyright (C) 2013 Scott Stephens.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of Florence nor the names of its contributors may
+ *    be used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ * OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 
 using System;
