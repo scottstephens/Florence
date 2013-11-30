c2fdaafee1f40c6e9e0c31d7c17a33c886d95642
Scott Stephens <stephens.js@gmail.com>
2013-11-30 15:22:35 -0600

    Show off initializers in creating plots.

--- examples/SimpleTestCommon/SimpleTestCommon.cs
+++ examples/SimpleTestCommon/SimpleTestCommon.cs
@@ -15,14 +15,12 @@ namespace SimpleTestCommon
             var x = new double[] { 1.0, 2.0, 3.0 };
             var y = new double[] { 1.0, 2.0, 3.0 };
             var z = new double[] { 0.0, 2.0, 4.0 };
-            var plot1 = new PointPlot();
-            plot1.AbscissaData = x;
-            plot1.OrdinateData = y;
+            var plot1 = new PointPlot() { AbscissaData = x, OrdinateData = y };
             plot.Add(plot1);
 
             Console.ReadLine();
 
-            var plot2 = new LinePlot(y, x);
+            var plot2 = new LinePlot() { AbscissaData = x, OrdinateData = y };
             plot.Title = "Test Plot 2";
             plot.XAxis1.Label = "X2";
             plot.YAxis1.Label = "Y2";
